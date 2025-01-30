cf4bc7001c1985a01f19477e5475a3bd42f419ad
AmineMaalej091102 <MohamedAmine.Maalej@esprit.tn>
2025-01-30 22:17:12 +0100

    ZooManagement class updated successfully :)

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,30 +1,8 @@
-import java.util.Scanner;
-
 public class ZooManagement {
     public static void main(String[] args) {
-        int nbrCages = 20;
-        String zooName = "my zoo";
-        System.out.println(zooName + " comporte " + nbrCages + " cages");
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Entrez le nom du zoo : ");
-        zooName = scanner.nextLine();
-
-        System.out.println("Entrez le nombre de cages (entier positif) : ");
-        while(!scanner.hasNextInt()) {
-            System.out.println("Veuillez entrer un nombre valide : ");
-            scanner.next();
-        }
-        nbrCages = scanner.nextInt();
-
-        while(nbrCages <= 0) {
-            System.out.println("Le nombre de cages doit être supérieur à 0. Réessayez : ");
-            nbrCages = scanner.nextInt();
-        }
-
-        System.out.println(zooName + " comporte " + nbrCages + " cages");
-
-        scanner.close();
+        Animal lion = new Animal("Felins", "Lion", 5, true );
+        Zoo myZoo = new Zoo("My Zoo", "Tunis", 20);
+        lion.displayAnimal();
+        myZoo.displayZoo();
     }
 }
